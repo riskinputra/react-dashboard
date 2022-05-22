52edb7dd32f02c35dd9e8bdd679c57f5ddfd4dde
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-05-22 21:55:47 +0700

    navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex item-center gap-5 pl-4 pt-4 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex item-center gap-5 pl-4 pt-4 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -18,7 +24,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && <>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => {setActiveMenu(false)}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link to="/" onClick={handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware /> <span>Shoppy</span>
           </Link>
 
@@ -39,7 +45,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={`${index}-${item.name}`}
-                  onClick={(() => {})}
+                  onClick={handleCloseSidebar}
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon}
